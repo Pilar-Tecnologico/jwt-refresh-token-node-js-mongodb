bede54deac5aea5d544d426d45427e3c413442b3
Eugemo <mariaeugeniamoreno@hotmail.com>
2021-06-23 23:37:50 -0300

    fix branch

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
 const dbName = 'PilarTecno'
-const dbUri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.qtcrz.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const dbUri = `mongodb+srv://${dbUser}:${dbPass}@pilartecno.6mrs6.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const mongooseOptions = {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true};
 
 
