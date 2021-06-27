02d0755e0ef6df7ca93b0b64a42f9d5c52f86baf
Eugemo <mariaeugeniamoreno@hotmail.com>
2021-06-27 17:08:26 -0300

    enable token

--- app/routes/current.routes.js
+++ app/routes/current.routes.js
@@ -4,12 +4,12 @@ const { authJwt } = require('../middlewares');
 module.exports = function(app) {
     app.post(
         "/current",
-         // [authJwt.verifyToken, authJwt.isUser],
+         [authJwt.verifyToken, authJwt.isUser],
         controller.postCurrent
     ) 
 
     app.get("/current",
-   // [authJwt.verifyToken, authJwt.isUser],
+    [authJwt.verifyToken, authJwt.isUser],
     controller.getCurrent
   );
 };
