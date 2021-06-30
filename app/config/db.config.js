bcb27871a3e0853b0b6d5a196e27e73a08061528
Leonardo Morales <leonardo.morales@tsoftglobal.com>
2021-06-30 11:39:08 -0300

    refactor in config database

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,24 @@
 const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASSWORD;
-const dbName = 'PilarTecno'
-const dbUri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.qtcrz.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const dbName = 'PilarTecno';
+
+const db = require("../models");
+const Role = require("../models/role.model");
+const dbUri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.ll5f4.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const mongooseOptions = {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true};
 
-
-module.exports = {
-    dbUri, mongooseOptions
+const dbConnection = async () => {
+    db.mongoose
+  .connect(dbUri, mongooseOptions)
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    Role.initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
 }
+
+
+module.exports = { dbConnection };
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const dbConfig = require("./app/config/db.config");
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 let corsOptions = {
   origin: "http://localhost:8081"
@@ -16,10 +19,13 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-const Role = db.role;
+// connect databases
+dbConfig.dbConnection();
 
-db.mongoose
+//const db = require("./app/models");
+//const Role = db.role;
+
+/*db.mongoose
   .connect(dbConfig.dbUri, dbConfig.mongooseOptions)
   .then(() => {
     console.log("Successfully connect to MongoDB.");
@@ -28,7 +34,7 @@ db.mongoose
   .catch(err => {
     console.error("Connection error", err);
     process.exit();
-  });
+  });*/
 
 // simple route
 app.get("/", (req, res) => {
@@ -39,44 +45,7 @@ app.get("/", (req, res) => {
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 3000;
+// listen for requests
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'user' to roles collection");
-      });
-
-      new Role({
-        name: "moderator"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'moderator' to roles collection");
-      });
-
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
-      });
-    }
-  });
-}
